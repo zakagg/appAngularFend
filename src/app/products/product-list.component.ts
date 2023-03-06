2cfa294e33dd5e8c01a53e250de00f5d437646a5
Zakaria Aggoun <zakagg@gmail.com>
2023-03-07 00:13:07 +0100

    second commit

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,45 @@
+
+import {Component} from '@angular/core'
+@Component({
+    selector:'pm-products',
+    templateUrl:'./product-list.component.html'
+
+})
+export class ProductListComponent{
+    pageTitle :string ="Product List!"
+    imageWidth:number=50;
+    imageMargin:number=2;
+    products: any[]=[
+        {
+          "productId": 1,
+          "productName": "Leaf Rake",
+          "productCode": "GDN-0011",
+          "releaseDate": "March 19, 2021",
+          "description": "Leaf rake with 48-inch wooden handle.",
+          "price": 19.95,
+          "starRating": 3.2,
+          "imageUrl": "assets/images/leaf_rake.png"
+        },
+        
+        {
+          "productId": 8,
+          "productName": "Saw",
+          "productCode": "TBX-0022",
+          "releaseDate": "May 15, 2021",
+          "description": "15-inch steel blade hand saw",
+          "price": 11.55,
+          "starRating": 3.7,
+          "imageUrl": "assets/images/saw.png"
+        },
+        {
+          "productId": 10,
+          "productName": "Video Game Controller",
+          "productCode": "GMG-0042",
+          "releaseDate": "October 15, 2020",
+          "description": "Standard two-button video game controller",
+          "price": 35.95,
+          "starRating": 4.6,
+          "imageUrl": "assets/images/xbox-controller.png"
+        }
+      ];
+}
